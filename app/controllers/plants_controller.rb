0b3bd1ffd81fa1e08a5f3a8ec2e6d1ce24c28fe8
James Berry <84353254+JamesRynnBerry@users.noreply.github.com>
2021-09-01 10:30:02 -0400

    Completed Assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,14 +1,26 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response 
 
   # GET /plants
   def index
     plants = Plant.all
     render json: plants
   end
-
+    
+def destroy
+  plant = find_plant
+    plant.destroy
+    head :no_content
+end
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
+    render json: plant
+  end
+
+  def update
+    plant = find_plant
+    plant.update(plant_params)
     render json: plant
   end
 
@@ -20,7 +32,18 @@ class PlantsController < ApplicationController
 
   private
 
+def find_plant
+  Plant.find_by(id: params[:id])
+end
+
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
 end
+
+
+  
+
+
+
+
